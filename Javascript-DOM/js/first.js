4404dbbc03877c76b6167184e7190edd1e607b9f
wasifaislam88 <wasifaislam88@gmail.com>
2024-12-25 00:46:51 +0600

    first time push

--- Javascript-DOM/js/first.js
+++ Javascript-DOM/js/first.js
@@ -22,3 +22,24 @@ secondSection.style.backgroundColor = 'tomato';
 //document.quaryselector
 
 
+
+
+// dIMPORTANT: ldfjlsdfj
+// NOTE: fdfd
+// BOOKMARK: fdfsdfdsf
+// PROBLEM: fdsfdsf
+// BUG: dfdsf
+// TODO: dfdsfdfsdf
+// FIXME:
+// WLINK:
+// VLINK:
+// COMMENT: hello this is Comment
+// STEP:
+
+//  NOTE: eta note
+// BUG: eta bug
+//NOTE: sathe to aro kichu line lekha ja
+//BUG:nahian    ar ha akta folder er moddhe 1000 ta subfolder... tar moddhe 10000 ta .js file. every js file er 
+
+
+
